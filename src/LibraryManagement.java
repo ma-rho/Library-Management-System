a858ed374351ca18d7cd5c30f4249f476949ab75
Melissa <mk22556@essex.ac.uk>
2024-11-19 11:34:22 +0000

    file names changed

--- src/LibraryManagement.java
+++ src/LibraryManagement.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class LibraryManagement {
+public class LibraryManagement (1){
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
